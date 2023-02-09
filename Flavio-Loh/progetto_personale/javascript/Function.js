5cf873056b489de41c80e4e71897e95b2d13eab4
Floh02 <flavioloh02@gmail.com>
2023-02-09 10:49:38 +0100

    update

--- Flavio-Loh/progetto_personale/javascript/Function.js
+++ Flavio-Loh/progetto_personale/javascript/Function.js
@@ -1,29 +1,61 @@
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const addToCartButton = document.querySelectorAll(".add-to-cart");
 
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    const product = {
-      name: this.dataset.name,
-      price: this.dataset.price
-    };
+addToCartButton.forEach(button => {
+  button.addEventListener("click", function() {
+    // Recupera i dati dall'elemento button
+    const name = this.dataset.name;
+    const price = this.dataset.price;
 
-    addToCart(product);
+    // Crea un oggetto per memorizzare i dati dell'articolo
+    const item = { name, price };
+
+    // Controlla se è già presente un carrello nella session storage
+    let cart = sessionStorage.getItem("cart");
+    if (cart) {
+        // Se è presente, lo converte in un oggetto JavaScript
+        cart = JSON.parse(cart);
+    } else {
+        // Se non è presente, crea un nuovo array
+        cart = [];
+    }
+
+    // Aggiunge l'articolo al carrello
+    cart.push(item);
+
+    // Memorizza il carrello nella session storage
+    sessionStorage.setItem("cart", JSON.stringify(cart));
   });
 });
 
-function addToCart(product) {
-  const row = document.createElement('tr');
 
-  // Crea due nuove celle (td) per il nome e il prezzo del prodotto
-  const nameCell = document.createElement('td');
-  nameCell.textContent = product.name;
-  row.appendChild(nameCell);
+let cart = sessionStorage.getItem("cart");
+if (cart) {
+    // Se esiste, lo converte in un oggetto JavaScript
+    cart = JSON.parse(cart);
+} else {
+    // Se non esiste, lo crea
+    cart = [];
+}
 
-  const priceCell = document.createElement('td');
-  priceCell.textContent = product.price;
-  row.appendChild(priceCell);
+// Recupera la tabella del carrello dalla pagina
+const cartTable = document.querySelector("#cart-items");
 
-  // Aggiunge la riga alla tabella del carrello
-  const cartTable = document.querySelector('#cart tbody');
-  cartTable.appendChild(row);
+// Itera sul carrello per creare le righe della tabella
+for (const item of cart) {
+    // Crea una riga per l'articolo
+    const row = document.createElement("tr");
+
+    // Crea le celle per i dati dell'articolo
+    const nameCell = document.createElement("td");
+    nameCell.textContent = item.name;
+
+    const priceCell = document.createElement("td");
+    priceCell.textContent = item.price;
+
+    // Aggiunge le celle alla riga
+    row.appendChild(nameCell);
+    row.appendChild(priceCell);
+
+
+    cartTable.appendChild(row);
 }
