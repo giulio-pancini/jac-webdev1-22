c527a4c2d4a5ea4abb2e95ac58360b4dcaebcad7
Federico Bonomi <federico11bonomi@gmail.com>
2023-02-12 10:09:08 +0100

    funzione elimina domande e risposte

--- federico-bonomi/7_progetto_personale/operazioni.js
+++ federico-bonomi/7_progetto_personale/operazioni.js
@@ -1,25 +1,32 @@
 let indice=0;
+let indiceRisposta=0;
 
 function inserisciDomanda()
 {
     indice++;
     const nuovaDomanda = document.createElement('li');
 
+    //creo il bottone per eliminare la domanda
+    const bottoneElimina=document.createElement("button");
+    bottoneElimina.setAttribute("onclick",`elimina("domanda${indice}")`);
+    bottoneElimina.innerText="Elimina";
+    bottoneElimina.setAttribute("class","bottoneElimina");
+    nuovaDomanda.appendChild(bottoneElimina);
+
+    //creo la data
     const d=new Date();
     const giorno=String(d.getDate()).padStart(2, '0');
     const mese = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
     const anno = d.getFullYear();
     const orario=`${d.getHours()}:${d.getMinutes()}`;
     const data=`${orario} del ${giorno}/${mese}/${anno}`;
-
-
-    nuovaDomanda.appendChild(creaSezione('nomeUtenteDomanda','strong'));
-
     const sezioneData=document.createElement('p');
     sezioneData.innerText=data;
     sezioneData.setAttribute("class","data");
-
     nuovaDomanda.appendChild(sezioneData);
+
+
+    nuovaDomanda.appendChild(creaSezione('nomeUtenteDomanda','strong'));
     nuovaDomanda.appendChild(creaSezione('titolo','h2'));
     nuovaDomanda.appendChild(creaSezione('inputDomanda', 'p'));
 
@@ -44,7 +51,7 @@ function inserisciDomanda()
     //creo input risposta
     const risposta = document.createElement("input");
     risposta.setAttribute("type", "text");
-    risposta.setAttribute("id", `risposta${indice}`);
+    risposta.setAttribute("id", `inputRisposta${indice}`);
     risposta.setAttribute("class", "inputRisposta");
     risposta.setAttribute("placeholder", "Risposta");
 
@@ -72,11 +79,18 @@ function inserisciDomanda()
 
     const domande = document.getElementById('domande');
     nuovaDomanda.setAttribute('class', 'domanda');
+    nuovaDomanda.setAttribute('id',`domanda${indice}`);
 
     nuovaDomanda.appendChild(form);
     nuovaDomanda.appendChild(bottoneRisposte);
     nuovaDomanda.appendChild(sezioneCommenti);
     domande.appendChild(nuovaDomanda);
+
+
+    //resetto il form
+    document.getElementById("nomeUtenteDomanda").value='';
+    document.getElementById("titolo").value='';
+    document.getElementById("inputDomanda").value='';
 }
 
 function creaSezione(inputId, tipo)
@@ -89,8 +103,14 @@ function creaSezione(inputId, tipo)
 
 function aggiungiRisposta(parametro)
 {
+    indiceRisposta++;
     const nuovaRisposta = document.createElement('li');
-    nuovaRisposta.appendChild(creaSezione(`nomeUtente${parametro}`,'strong'));
+    //bottone elimina
+    const bottoneElimina=document.createElement("button");
+    bottoneElimina.setAttribute("onclick",`elimina("risposta${indiceRisposta}")`);
+    bottoneElimina.innerText="Elimina";
+    bottoneElimina.setAttribute("class","bottoneElimina");
+    nuovaRisposta.appendChild(bottoneElimina);
 
     const d=new Date();
     const giorno=String(d.getDate()).padStart(2, '0');
@@ -102,14 +122,18 @@ function aggiungiRisposta(parametro)
     sezioneData.innerText=data;
     sezioneData.setAttribute("class","data");
     nuovaRisposta.appendChild(sezioneData);
-    
-    nuovaRisposta.appendChild(creaSezione(`risposta${parametro}`,'p'));
+    nuovaRisposta.appendChild(creaSezione(`nomeUtente${parametro}`,'strong'));
+    nuovaRisposta.appendChild(creaSezione(`inputRisposta${parametro}`,'p'));
 
     const risposte = document.getElementById(`ul${parametro}`);
     nuovaRisposta.setAttribute('class', `risposta`);
-    nuovaRisposta.setAttribute('id', `risposta${parametro}`);
+    nuovaRisposta.setAttribute('id', `risposta${indiceRisposta}`);
 
     risposte.appendChild(nuovaRisposta);
+
+    //resetto il form
+    document.getElementById(`nomeUtente${parametro}`).value='';
+    document.getElementById(`inputRisposta${parametro}`).value='';
 }
 
 function nascondi(parametro, bottoneForm)
@@ -140,4 +164,9 @@ function nascondiRisposte(parametro, bottoneCommenti)
         stato.setAttribute("class","scompari");
         const commenti=document.getElementById(bottoneCommenti).innerText="Mostra Risposte";
     }
+}
+
+function elimina(indice)
+{
+    document.getElementById(indice).remove();
 }
