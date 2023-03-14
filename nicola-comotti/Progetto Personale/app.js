e6389a331afadaa282393fee98542188afed22db
NicolaComotti <nicola.comotti46@gmail.com>
2023-03-14 17:29:52 +0100

    Aggiunta e Rimozione dimaniche delle schede

--- nicola-comotti/Progetto Personale/app.js
+++ nicola-comotti/Progetto Personale/app.js
@@ -26,6 +26,60 @@ function logout() {
     window.location.href="login-page.html";
 }
 
+const schede = [];
+
+function addScheda() {
+
+    const newScheda = document.createElement('li');
+
+    const inputNome = document.getElementById('nomeScheda');
+    const nome = inputNome.value;
+
+    const testoNome = document.createElement('h2');
+    testoNome.innerText = nome;
+
+    const buttonRimuovi = document.createElement('button');
+    const imgBin = document.createElement('img');
+    imgBin.setAttribute('src', 'Utils/white-recycle-bin.png', 'alt', 'Elimina');
+    buttonRimuovi.appendChild(imgBin);
+    buttonRimuovi.setAttribute("onclick", "deleteScheda(this)");
+    buttonRimuovi.setAttribute("class", "delete-button");
+    newScheda.appendChild(testoNome);
+    newScheda.appendChild(buttonRimuovi);
+    
+    newScheda.setAttribute('class','scheda');
+    const idScheda = testoNome.innerText.replace(/ /g,'');
+    newScheda.setAttribute("id", idScheda);
+
+    document.getElementById('listaSchede').appendChild(newScheda);
+
+    const selectSchede = document.getElementById('sceltaScheda');
+    const optionScheda = document.createElement('option');
+    optionScheda.setAttribute('value',idScheda);
+    optionScheda.appendChild(document.createTextNode(testoNome.innerText));
+    selectSchede.appendChild(optionScheda);
+
+    schede.push(idScheda);
+
+    inputNome.value = '';
+
+}
+
+function deleteScheda (elem) {
+
+    const scheda = document.getElementById(elem.parentNode.id);
+    scheda.style.display = 'none';
+
+    schede.splice(schede.indexOf(elem.parentNode.id),1);
+
+    const selectSchede = document.getElementById("sceltaScheda");
+    for (var i=0; i<selectSchede.length; i++) {
+    if (selectSchede.options[i].value == elem.parentNode.id)
+        selectSchede.remove(i);
+    }
+
+}
+
 function addExe() {
 
     const newExe = document.createElement('li');
@@ -58,6 +112,7 @@ function addExe() {
     imgBin.setAttribute('src', 'Utils/white-recycle-bin.png', 'alt', 'Elimina');
     buttonRimuovi.appendChild(imgBin);
     buttonRimuovi.setAttribute("onclick", "this.parentElement.style.display = 'none';");
+    buttonRimuovi.setAttribute("class", "delete-button");
 
     newExe.appendChild(testoNome);
     newExe.appendChild(testoPeso);
@@ -68,6 +123,13 @@ function addExe() {
 
     newExe.setAttribute('class', 'esercizio');
 
+    for (const valueScheda of schede) {
+        if(valueScheda == scheda) {
+            document.getElementById(valueScheda).appendChild(newExe);
+            break;
+        }
+    }
+/*
     const schedaPush = document.getElementById('schedaPush');
     const schedaPull = document.getElementById('schedaPull');
     const schedaLegs = document.getElementById('schedaLegs');
@@ -86,10 +148,8 @@ function addExe() {
             alert("NESSUNA SCHEDA SELEZIONATA!!");
             break;
     }
-
+*/
 }
-
-// function addScheda() {}
 
 async function fetchEsercizi() {
     const response = await fetch("http://localhost:8080/esercizioAPI/listaesercizi");
